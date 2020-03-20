fa61c32377c0358f9ecdfbd784ad6dff4b31d85e
Ozan <ozanbolel@outlook.com>
2020-03-20 20:54:16 +0300

    server url changed

--- core/config/server.ts
+++ core/config/server.ts
@@ -1,5 +1,5 @@
 import { isProduction } from "./isProduction";
 
 export const server = {
-  uri: isProduction ? "https://tooldeck-api.herokuapp.com:4000/api" : "http://localhost:4000/api"
+  uri: isProduction ? "https://tooldeck-api.herokuapp.com/api" : "http://localhost:4000/api"
 };
