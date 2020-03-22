040ed66a334be48a48f0e360f1a5802246b5acf4
Ozan <ozanbolel@outlook.com>
2020-03-22 18:35:40 +0300

    try for https twitter oauth

--- components/landing/Index/Index.tsx
+++ components/landing/Index/Index.tsx
@@ -1,12 +1,65 @@
 import * as React from "react";
 import css from "./Index.module.scss";
 import { oauth } from "core/config";
+import { hmacsign } from "./xx";
+import fetch from "isomorphic-fetch";
+
+const variables = {
+  TWITTER_API_KEY: "hQuC1wx9YQETTKjrAjcKWAN5j",
+  TWITTER_SECRET_KEY: "PRCVaknKyXjsP9scaH9B37qiyuuaRaN9bjKWyJKcGi8T0c3hsr"
+};
 
 const Index: React.FC = () => {
+  const onClickGithub = () => {
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${oauth.github.clientId}`;
+  };
+
+  const onClickTwitter = async () => {
+    const url = "https://api.twitter.com/oauth/request_token";
+
+    const method = "POST";
+
+    const params = {
+      oauth_nonce: "nonce" + new Date().getTime(),
+      oauth_callback: encodeURIComponent("https://tooldeck.now.sh/auth/twitter"),
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_timestamp: new Date().getTime(),
+      oauth_consumer_key: variables.TWITTER_API_KEY,
+      oauth_version: "1.0"
+    };
+
+    const signed = hmacsign(method, url, params, variables.TWITTER_SECRET_KEY);
+
+    console.log(signed);
+
+    const authorizationString =
+      "OAuth " +
+      `oauth_nonce="${"nonce" + new Date().getTime()}", ` +
+      `oauth_callback="${encodeURIComponent("https://tooldeck.now.sh/auth/twitter")}", ` +
+      `oauth_signature_method="HMAC-SHA1", ` +
+      `oauth_timestamp="${new Date().getTime()}", ` +
+      `oauth_consumer_key="${variables.TWITTER_API_KEY}", ` +
+      `oauth_signature="${encodeURIComponent(signed)}", ` +
+      `oauth_version="1.0"`;
+
+    const response = await fetch(url + "?oauth_callback=https://tooldeck.now.sh/auth/twitter", {
+      method,
+      headers: { Authorization: authorizationString }
+    });
+
+    console.log(response);
+  };
+
   return (
-    <a href={`https://github.com/login/oauth/authorize?client_id=${oauth.github.clientId}`} target="_self">
-      <button className={css.withGithub}>Login with GitHub</button>
-    </a>
+    <div>
+      <button className={css.withGithub} onClick={() => onClickGithub()}>
+        Login with GitHub
+      </button>
+
+      <button className={css.withGithub} onClick={() => onClickTwitter()}>
+        Login with Twitter
+      </button>
+    </div>
   );
 };
 
---
+++ pages/auth/twitter.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { TPage } from "core/types";
+
+const TwitterRedirect: TPage = () => {
+  return <h1>twitter auth...</h1>;
+};
+
+export default TwitterRedirect;
